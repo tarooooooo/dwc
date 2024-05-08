5aaaf137113e4a76a734ba3ed823311d1f5bd725
ryutaro maeda <r.maeda@asaburo.com>
2024-05-08 19:58:43 +0900

    first commit

---
+++ javascript/12章/js/script.js
@@ -0,0 +1,29 @@
+// （API_KEY には、"取得したAPIキー" を記述）
+const API_KEY = "650fd1c46668a58e4177b3cd870138f4";
+$(function(){
+    $('#btn').on('click', function() {
+        // 入力された都市名でWebAPIに天気情報をリクエスト
+        $.ajax({
+            url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+            dataType : 'jsonp',
+        }).done(function (data){
+// 位置
+            $('#place').text(data.name);
+            // 最高気温
+            $('#temp_max').text(data.main.temp_max);
+            // 最低気温
+            $('#temp_min').text(data.main.temp_min);
+            //湿度
+            $('#humidity').text(data.main.humidity);
+            //風速
+            $('#speed').text(data.wind.speed);
+            // 天気
+            $('#weather').text(data.weather[0].main);
+            // 天気アイコン
+            $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+            $('img').attr("alt",data.weather[0].main);
+        }).fail(function (data) {
+            alert('通信に失敗しました。');
+        });
+    });
+});
